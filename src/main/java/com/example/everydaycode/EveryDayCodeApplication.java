0b5abf834288d017423a03f3d9e45373d75cfc3e
Administrator <190816935@qq.com>
2023-04-12 11:19:51 +0800

    首次提交

---
+++ src/main/java/com/example/everydaycode/EveryDayCodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.everydaycode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EveryDayCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EveryDayCodeApplication.class, args);
+    }
+
+}
